fb831d7559f5806bc6c51ad7740013405b2e09e3
Brody Davison <bbd4327@rit.edu>
2018-02-21 09:52:58 -0500

    egg

--- Egg/Egg/Game1.cs
+++ Egg/Egg/Game1.cs
@@ -18,7 +18,7 @@ namespace Egg
             Content.RootDirectory = "Content";
         }
 
-        //comment
+        //egg
 
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
