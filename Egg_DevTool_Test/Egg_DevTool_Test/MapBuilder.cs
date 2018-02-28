c49c98e23853c04e439dfbf5c7c958c7060f5dfd
Beatrice <Beatrice@DESKTOP-FOGA9V5>
2018-02-28 13:24:00 -0500

    starting work on assist tool

---
+++ Egg_DevTool_Test/Egg_DevTool_Test/MapBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Egg_DevTool_Test
+{
+    public partial class MapBuilder : Form
+    {
+        public MapBuilder()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //do this
+        }
+
+        string output = 
+            "This is me testing writing a line of text into a text file." + Environment.NewLine +
+            "This is hopefully a second line.";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter writer = new StreamWriter("eggTest.txt");
+            writer.Write(output);
+            writer.Close();
+        }
+    }
+}
