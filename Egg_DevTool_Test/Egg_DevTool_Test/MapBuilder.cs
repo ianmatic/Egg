03002a719761ec1a3d546f2f8d2381644dff2aac
jtv6445 <jtv6445@IGM2350-09>
2018-02-28 14:18:13 -0500

    Neutrals tab setup almost complete -- start working on the code along the back-end and hook everything up

--- Egg_DevTool_Test/Egg_DevTool_Test/MapBuilder.cs
+++ Egg_DevTool_Test/Egg_DevTool_Test/MapBuilder.cs
@@ -13,19 +13,15 @@ namespace Egg_DevTool_Test
 {
     public partial class MapBuilder : Form
     {
-        public MapBuilder()
-        {
-            InitializeComponent();
-        }
+        #region Text Output
 
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-            //do this
-        }
+        // for each item in along the text file, have some variable saved off invisibly
+        // so that we can make a big loop and send all of those into a series of strings
+        // which we can then combine into the output and print to a text file.
 
-        string output = 
-            "This is me testing writing a line of text into a text file." + Environment.NewLine +
-            "This is hopefully a second line.";
+        string output =
+    "This is me testing writing a line of text into a text file." + Environment.NewLine +
+    "This is hopefully a second line.";
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -33,5 +29,42 @@ namespace Egg_DevTool_Test
             writer.Write(output);
             writer.Close();
         }
+        #endregion
+
+
+        public MapBuilder()
+        {
+            InitializeComponent();
+            //set up a default function to change it so every button in the placement container has no text
+        }
+
+
+        #region Etcetera Clicked Functions
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            //do this
+        }
+
+
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button_click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
     }
 }
