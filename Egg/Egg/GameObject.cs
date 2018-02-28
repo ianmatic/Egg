12bb652be7bfa07cd93976d452fba3d04498cb42
Brody Davison <bbd4327@rit.edu>
2018-02-28 13:57:18 -0500

    Base gameobject class started. Next step is to have the other entity classes implement GameObject correctly.

---
+++ Egg/Egg/GameObject.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Egg
+{
+    abstract class GameObject
+    {
+        private Rectangle hitbox;
+        private Texture2D defaultSprite;
+        private int drawLevel;
+        private bool isActive;
+
+        //The order/layer the sprite should be drawn on screen. 
+        public int DrawLevel
+        {
+            get { return this.drawLevel; }
+            set { this.drawLevel = value; }
+        }
+
+        public Texture2D DefaultSprite
+        {
+            get { return this.defaultSprite; }
+        }
+
+        public Rectangle Hitbox
+        {
+            get { return this.hitbox; }
+        }
+
+        public bool IsActive
+        {
+            get { return this.isActive}
+            set { this.isActive = value; }
+        } 
+
+
+
+        public abstract void Draw(SpriteBatch sb);
+
+    }
+}
