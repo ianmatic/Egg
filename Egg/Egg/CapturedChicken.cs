97f604da6dfa53ff66f833aabc742cb387712584
Brody Davison <bbd4327@rit.edu>
2018-03-19 09:31:46 -0400

    DrawLevel implemented, use PotatoDebugging() to test

--- Egg/Egg/CapturedChicken.cs
+++ Egg/Egg/CapturedChicken.cs
@@ -8,11 +8,21 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Egg
 {
+    //Being used to debug the draw system, feel free to make structural changes later
     class CapturedChicken : GameObject
     {
+        Color color;
+
+        public CapturedChicken(int drawLevel, Texture2D defaultSprite, Rectangle hitbox, Color color)
+        {
+            this.drawLevel = drawLevel;
+            this.defaultSprite = defaultSprite;
+            this.hitbox = hitbox;
+            this.color = color;
+        }
         public override void Draw(SpriteBatch sb)
         {
-            
+            sb.Draw(defaultSprite, hitbox, this.color);
         }
         public override void Movement()
         {
