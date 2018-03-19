37116697408c89cc08f0e7cb5d106f5e77055a48
Brody Davison <bbd4327@rit.edu>
2018-03-19 09:49:14 -0400

    Started Level and Screen

---
+++ Egg/Egg/Level.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Egg
+{
+    //Represents a level of the game containing a variable amount of screens
+    class Level
+    {
+        List<Screen> screenList;
+        int remainingChickens;
+
+        public int RemainingChickens
+        {
+            get { return remainingChickens; }
+        }
+
+        public Level(List<Screen> listOfScreens, int totalChickens)
+        {
+            this.screenList = listOfScreens;
+            this.remainingChickens = totalChickens;
+        }
+
+    }
+}
