8ecebcf6707de5691a6c153264c6427cc035862e
Brody Davison <bbd4327@rit.edu>
2018-03-25 16:39:25 -0400

    oof

--- Egg/Egg/Screen.cs
+++ Egg/Egg/Screen.cs
@@ -11,12 +11,50 @@ namespace Egg
     {
         Tile[,] tileList;
         Enemy[] listOfEnemies;
+        int tileX;
+        int tileY;
+
         int lengthX; // (Tiles in X direction)*(Length of a side of a single tile)
         int lengthY; // (Tiles in Y direction)*(Length of a side of a single tile)
 
         public Screen(string textFile)
         {
             
+        }
+
+        public void LoadTiles(Player p)
+        {
+            const int SCREEN_LENGTH = 16;
+            const int SCREEN_WIDTH = 9;
+
+            //Calculate player's tile
+            int playerTileX = (int)Math.Round((double)p.Hitbox.X / SCREEN_LENGTH);
+            int playerTileY = (int)Math.Round((double)p.Hitbox.Y / SCREEN_WIDTH);
+
+            //the loop
+
+            for (int row = 0; row < tileY; row++)
+            {               
+                for(int column = 0; column < tileX; column++)
+                {
+                    if (row > (playerTileY - 6) && row < (playerTileY + 6))
+                    {
+                        if (column > (playerTileX - 9) && column < (playerTileX + 9))
+                        {
+                            tileList[row, column].IsActive = true;
+                        }
+                        else
+                        {
+                            tileList[row, column].IsActive = false;
+                        }
+                    }
+                    else
+                    {
+                        tileList[row, column].IsActive = false;
+                    }
+                }
+            } //End loop
+
         }
     }
 }
