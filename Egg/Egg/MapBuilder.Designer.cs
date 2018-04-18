41727d30e2d1097d062a97e8e047327b2ca99d2d
Beatrice <Beatrice@DESKTOP-FOGA9V5>
2018-04-17 21:58:23 -0400

    tablet exporting fixed

--- Egg/Egg/MapBuilder.Designer.cs
+++ Egg/Egg/MapBuilder.Designer.cs
@@ -43,9 +43,10 @@
             // 
             // txtFile
             // 
-            this.txtFile.Location = new System.Drawing.Point(40, 571);
+            this.txtFile.Location = new System.Drawing.Point(53, 703);
+            this.txtFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtFile.Name = "txtFile";
-            this.txtFile.Size = new System.Drawing.Size(156, 20);
+            this.txtFile.Size = new System.Drawing.Size(207, 22);
             this.txtFile.TabIndex = 31;
             this.txtFile.Text = "<text file name>";
             // 
@@ -53,9 +54,10 @@
             // 
             this.rad5.AutoSize = true;
             this.rad5.Checked = true;
-            this.rad5.Location = new System.Drawing.Point(38, 375);
+            this.rad5.Location = new System.Drawing.Point(51, 462);
+            this.rad5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rad5.Name = "rad5";
-            this.rad5.Size = new System.Drawing.Size(58, 17);
+            this.rad5.Size = new System.Drawing.Size(74, 21);
             this.rad5.TabIndex = 30;
             this.rad5.TabStop = true;
             this.rad5.Text = "Normal";
@@ -64,28 +66,32 @@
             // rad4
             // 
             this.rad4.AutoSize = true;
-            this.rad4.Location = new System.Drawing.Point(38, 466);
+            this.rad4.Location = new System.Drawing.Point(51, 574);
+            this.rad4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rad4.Name = "rad4";
-            this.rad4.Size = new System.Drawing.Size(60, 17);
+            this.rad4.Size = new System.Drawing.Size(74, 21);
             this.rad4.TabIndex = 28;
             this.rad4.Text = "Moving";
             this.rad4.UseVisualStyleBackColor = true;
             // 
             // button151
             // 
-            this.button151.Location = new System.Drawing.Point(38, 613);
+            this.button151.Location = new System.Drawing.Point(51, 754);
+            this.button151.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button151.Name = "button151";
-            this.button151.Size = new System.Drawing.Size(163, 64);
+            this.button151.Size = new System.Drawing.Size(217, 79);
             this.button151.TabIndex = 27;
             this.button151.Text = "Export to Text";
             this.button151.UseVisualStyleBackColor = true;
+            this.button151.Click += new System.EventHandler(this.Export);
             // 
             // rad3
             // 
             this.rad3.AutoSize = true;
-            this.rad3.Location = new System.Drawing.Point(38, 445);
+            this.rad3.Location = new System.Drawing.Point(51, 548);
+            this.rad3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rad3.Name = "rad3";
-            this.rad3.Size = new System.Drawing.Size(86, 17);
+            this.rad3.Size = new System.Drawing.Size(112, 21);
             this.rad3.TabIndex = 25;
             this.rad3.Text = "Non-Collision";
             this.rad3.UseVisualStyleBackColor = true;
@@ -93,43 +99,50 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(127, 514);
+            this.label3.Location = new System.Drawing.Point(169, 633);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.Size = new System.Drawing.Size(92, 17);
             this.label3.TabIndex = 24;
             this.label3.Text = "Tablet Width:";
             // 
             // tabletWidth
             // 
-            this.tabletWidth.Location = new System.Drawing.Point(130, 536);
+            this.tabletWidth.Location = new System.Drawing.Point(173, 660);
+            this.tabletWidth.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabletWidth.Name = "tabletWidth";
-            this.tabletWidth.Size = new System.Drawing.Size(66, 20);
+            this.tabletWidth.Size = new System.Drawing.Size(87, 22);
             this.tabletWidth.TabIndex = 23;
             this.tabletWidth.Text = "15";
+            this.tabletWidth.TextChanged += new System.EventHandler(this.HeightWidthChange);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(37, 514);
+            this.label2.Location = new System.Drawing.Point(49, 633);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.Size = new System.Drawing.Size(97, 17);
             this.label2.TabIndex = 22;
             this.label2.Text = "Tablet Height:";
             // 
             // tabletHeight
             // 
-            this.tabletHeight.Location = new System.Drawing.Point(40, 536);
+            this.tabletHeight.Location = new System.Drawing.Point(53, 660);
+            this.tabletHeight.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabletHeight.Name = "tabletHeight";
-            this.tabletHeight.Size = new System.Drawing.Size(64, 20);
+            this.tabletHeight.Size = new System.Drawing.Size(84, 22);
             this.tabletHeight.TabIndex = 21;
             this.tabletHeight.Text = "10";
+            this.tabletHeight.TextChanged += new System.EventHandler(this.HeightWidthChange);
             // 
             // rad2
             // 
             this.rad2.AutoSize = true;
-            this.rad2.Location = new System.Drawing.Point(38, 422);
+            this.rad2.Location = new System.Drawing.Point(51, 519);
+            this.rad2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rad2.Name = "rad2";
-            this.rad2.Size = new System.Drawing.Size(96, 17);
+            this.rad2.Size = new System.Drawing.Size(124, 21);
             this.rad2.TabIndex = 20;
             this.rad2.Text = "Non-Damaging";
             this.rad2.UseVisualStyleBackColor = true;
@@ -137,18 +150,19 @@
             // rad1
             // 
             this.rad1.AutoSize = true;
-            this.rad1.Location = new System.Drawing.Point(38, 398);
+            this.rad1.Location = new System.Drawing.Point(51, 490);
+            this.rad1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rad1.Name = "rad1";
-            this.rad1.Size = new System.Drawing.Size(73, 17);
+            this.rad1.Size = new System.Drawing.Size(93, 21);
             this.rad1.TabIndex = 19;
             this.rad1.Text = "Damaging";
             this.rad1.UseVisualStyleBackColor = true;
             // 
             // Mappy
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1186, 709);
+            this.ClientSize = new System.Drawing.Size(1581, 873);
             this.Controls.Add(this.txtFile);
             this.Controls.Add(this.rad5);
             this.Controls.Add(this.rad4);
@@ -160,7 +174,7 @@
             this.Controls.Add(this.tabletHeight);
             this.Controls.Add(this.rad2);
             this.Controls.Add(this.rad1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Mappy";
             this.Text = "MapBuilder";
             this.Load += new System.EventHandler(this.MapBuilder_Load);
